416600dd018fd6e90883c720ed2b236fb6fa9f46
Divakar <divakar2163@gmail.com>
2023-01-21 15:21:25 +0530

    vue watcher

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,36 +1,21 @@
 <template>
-  <div>
-     <h1>Hi there, its watchers here</h1>
-    <br>
-    <h2>Range is (0 - 20)</h2>
-    <h2>Count is {{count}}</h2>
-    <button @click="count+=2">+</button>
-    <button @click="count-=2">-</button>
-  </div>
- 
+    <div>
+        <h1>{{msg}}</h1>
+
+    </div>
 </template>
 
 <script>
-
-export default ({
-  name:'App',
-  data(){
-    return {
-      count:0,
-
+    export default {
+        name: 'App',
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
     }
-  },
-  methods:{},
-  computed:{},
-  watch:{
-    // watchers deep and imemdiate have to learnt 
-    count(newValue,oldValue){
-      // console.log(`new is ${newValue} old is ${oldValue}`);
-      if(newValue>oldValue && newValue==16){
-        
-        alert(`You are at the limit ${newValue} ${oldValue}`)
-      }
-    }
-  }
-})
 </script>
+
+<style scoped>
+
+</style>
