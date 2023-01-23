cecc50400c167ab597f2ddb2b3328728a044d01d
Divakar <divakar2163@gmail.com>
2023-01-23 06:15:49 +0530

    vue comps started

---
+++ components-practice/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+
+<div>
+  <h1>Hi from Components project</h1>
+<Greet/>
+</div>
+
+</template>
+
+<script>
+
+import Greet from './components/Greet.vue'
+export default {
+  name: 'App',
+  components:{
+    Greet,
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ components-practice/src/components/Greet.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>Hello Diva</h2>
+        <h3>Can you see me??</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'greetIngs'
+    }
+</script>
+
+<style scoped>
+
+</style>
